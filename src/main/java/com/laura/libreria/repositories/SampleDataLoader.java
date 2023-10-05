8ed0577e83e811ba67a946d20ab45f6a4cb025c9
Laura Contreras <lauracontreras@MacBook-Pro-de-Laura.local>
2023-10-05 20:34:54 +0200

    agregando libros a través de la clase sample data loader

---
+++ src/main/java/com/laura/libreria/repositories/SampleDataLoader.java
@@ -0,0 +1,32 @@
+package com.laura.libreria.repositories;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import jakarta.annotation.PostConstruct;
+
+@Component
+public class SampleDataLoader {
+    private BookRepository bookRepository;
+
+    @Autowired
+    public SampleDataLoader(BookRepository bookRepository){
+        this.bookRepository = bookRepository;
+    }
+
+    @PostConstruct
+    public void loadSampleData(){
+        bookRepository.saveAll(List.of(
+            new Book("Una habitación propia", "Virginia Woolf", "Ensayo"),
+            new Book("Un mago de Terramar", "Ursula K. Leguin", "Fantasia"),
+            new Book("Los desposeídos", "Ursula K. Leguin", "Fantasia"),
+            new Book("Las flores del mal", "Charles Baudelaire", "Poesia"),
+            new Book ("La comunidad del anillo", "J.R.R. Tolkien", "Fantasía"),
+            new Book ("Learn Software Development", "Mary Poppendieck", "Software"),
+            new Book("Object Design", "Rebbeca Wirfs-Brock", "Software")
+        ));
+    }
+    
+}
