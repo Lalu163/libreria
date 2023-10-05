a9950630b10206a8912c3e05b37da1db2a0fa457
Laura Contreras <lauracontreras@MacBook-Pro-de-Laura.local>
2023-10-05 13:28:21 +0200

    creado controlador y primer test pasado

--- src/main/java/com/laura/libreria/controllers/HomeController.java
+++ src/main/java/com/laura/libreria/controllers/HomeController.java
@@ -1,8 +1,12 @@
 package com.laura.libreria.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
-    
+    @GetMapping("/")
+    public String home(){
+        return "home";
+    }
 }
