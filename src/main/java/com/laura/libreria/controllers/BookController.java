86317704bffd02aa3bacbc8fb5f5e63dd07395b7
Laura Contreras <lauracontreras@MacBook-Pro-de-Laura.local>
2023-10-05 20:11:42 +0200

    creado llamada del book controller y pasado el test

---
+++ src/main/java/com/laura/libreria/controllers/BookController.java
@@ -0,0 +1,31 @@
+package com.laura.libreria.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+import com.laura.libreria.repositories.Book;
+import com.laura.libreria.repositories.BookRepository;
+
+import java.util.List;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class BookController {
+    private BookRepository bookRepository;
+
+    @Autowired
+    public BookController(BookRepository bookRepository){
+        this.bookRepository = bookRepository;
+    }
+
+    @GetMapping("/books")
+    String listBooks(Model model){
+        List<Book> books = (List<Book>) bookRepository.findAll();
+        model.addAttribute("title", "Book list");
+        model.addAttribute("books", books);
+        return "books/all";
+    }
+    
+}
--- src/main/java/com/laura/libreria/repositories/BookRepository.java
+++ src/main/java/com/laura/libreria/repositories/BookRepository.java
@@ -1,2 +1,8 @@
-export interface BookRepository.java {
+package com.laura.libreria.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookRepository extends CrudRepository<Book, Long> {
 }
